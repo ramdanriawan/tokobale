f70f5347706fac763532c006fdac160731d12f1a
ramdan riawan <ramdanriawan3@gmail.com>
2019-07-27 13:31:11 +0700

    penambahan header table dan tanda tangan

--- admin/dashboard/print.php
+++ admin/dashboard/print.php
@@ -30,60 +30,79 @@ if (isset($_GET['start_date']) && isset($_GET['end_date']))
 
 ?>
 
-<div class="table-responsive" style="padding-top: 5px">
-					<table class="table table-bordered table-hover" id="dataTables-example">
-						<thead>
-							<tr style="background-color: #ccc">
-								<th>Nama Produk</th>
-								<th>Size</th>
-								<th>Jumlah</th>
-								<th>Harga Satuan</th>
-								<th>Diskon</th>
-								<th>Total - Diskon</th>
-							</tr>
-						</thead>
-							<tbody>
-							<?php 
-							$jumlah = 0;
-							$totalHargaSemua = 0;
-							foreach ($orders_detail as $order_detail)
-							{
-								foreach ($order_detail as $order_detail2)
-								{
-									$produk = selectWhere("nama_produk, diskon", "produk", "kodeproduk='{$order_detail2['kodeproduk']}'");
+<div style="border: 2px dotted black; border-radius: 5px;">
+	<div style="text-align: center;">
+		<strong>Toko Bale</strong> <br>
+		<strong>Laporan Penjualan</strong> <br>
+		<strong>Periode <?php echo $start_date . " S/d " . $end_date; ?></strong>
+	</div>
+</div>
 
-									$jumlah += $order_detail2['jumlah'];
-									$totalHarga = $order_detail2['total'] - ($order_detail2['total'] * ($produk[0]['diskon'] / 100));
-									$totalHargaSemua += $totalHarga;
+<div style="padding-top: 5px;">
+	<center>
+		<table>
+			<thead>
+				<tr style="background-color: #ccc">
+					<th>Nama Produk</th>
+					<th>Size</th>
+					<th>Jumlah</th>
+					<th>Harga Satuan</th>
+					<th>Diskon</th>
+					<th>Total - Diskon</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php 
+			$jumlah = 0;
+			$totalHargaSemua = 0;
+			foreach ($orders_detail as $order_detail)
+			{
+				foreach ($order_detail as $order_detail2)
+				{
+					$produk = selectWhere("nama_produk, diskon", "produk", "kodeproduk='{$order_detail2['kodeproduk']}'");
 
-echo "
-									<tr>
-										<td>{$produk[0]['nama_produk']}</td>
-										<td>{$order_detail2['size']}</td>
-										<td>{$order_detail2['jumlah']}</td>
-										<td>" . toRupiah($order_detail2['harga_satuan']) . "</td>
-										<td>{$produk[0]['diskon']}%</td>
-										<td>" . toRupiah($totalHarga) . "</td>
-									</tr>
-";	
-								}
+					$jumlah += $order_detail2['jumlah'];
+					$totalHarga = $order_detail2['total'] - ($order_detail2['total'] * ($produk[0]['diskon'] / 100));
+					$totalHargaSemua += $totalHarga;
 
-							}
+	echo "
+					<tr>
+						<td>{$produk[0]['nama_produk']}</td>
+						<td>{$order_detail2['size']}</td>
+						<td>{$order_detail2['jumlah']}</td>
+						<td>" . toRupiah($order_detail2['harga_satuan']) . "</td>
+						<td>{$produk[0]['diskon']}%</td>
+						<td>" . toRupiah($totalHarga) . "</td>
+					</tr>
+	";	
+				}
 
-							?>	
-								<tr>
-									<td></td>
-									<td></td>
-									<td><?php echo $jumlah; ?></td>
-									<td></td>
-									<td></td>
-									<td><?php echo toRupiah($totalHargaSemua); ?></td>
-								</tr>
-							</tbody>	
-						</table>
-					</div>
+			}
 
-					<script type="text/javascript">
-						window.print();
-						window.close();
-					</script>
+			?>	
+				<tr>
+					<td></td>
+					<td></td>
+					<td><?php echo $jumlah; ?></td>
+					<td></td>
+					<td></td>
+					<td><?php echo toRupiah($totalHargaSemua); ?></td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr style="padding-top: 50px;">
+					<td>
+						<span style="margin-bottom: -120px; display: inline-block;">Jambi, <?php echo date('d-m-Y'); ?></span><br>
+						<img src="../../images/tanda_tangan.png" with="100" height="100" style="margin-left: -50px; margin-bottom: -20px;"><br>
+						<span style="margin-top: -5px; display: inline-block;">Febby Febriana</span>
+					</td>
+				</tr>
+			</tfoot>	
+		</table>
+	</center>
+</div>
+
+<script type="text/javascript">
+	// window.print();
+	// window.close();
+</script>
