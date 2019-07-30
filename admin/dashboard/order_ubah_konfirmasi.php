593f61c19977c6132a1355bb33c9fa7e442380cb
ramdan riawan <ramdanriawan3@gmail.com>
2019-07-30 12:50:31 +0700

    8. perbaikan menu revisi konfirmasi pembayaran

--- admin/dashboard/order_ubah_konfirmasi.php
+++ admin/dashboard/order_ubah_konfirmasi.php
@@ -8,6 +8,6 @@ echo $_GET['id_order'];
 update('orders', ['status_konfirmasi' => 'Disetujui', 'status_order' => 'Sudah Dibayar'], "id_order='$_GET[id_order]'");
 
 //arahkan balik ke halaman konfirmasi
-header("Location: ./konfirmasipembayaran.php?halaman=konfirmasipembayaran");
+header("Location: $_SERVER[HTTP_REFERER]");
 
  ?>
